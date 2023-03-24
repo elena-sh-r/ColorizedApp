2204be44afc4dc7ad38f13f4904c320d1ba9ea08
Elena Sharipova <elena.sh.r@yandex.ru>
2023-03-24 18:01:13 +0300

    feat: colorized app implemented

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+final class ViewController: UIViewController {
+    // MARK: - IB Outlets
+    @IBOutlet var currentColorSwatch: UIView!
+    
+    @IBOutlet var redColorLabel: UILabel!
+    @IBOutlet var greenColorLabel: UILabel!
+    @IBOutlet var blueColorLabel: UILabel!
+    
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
+    // MARK: - IB Actions
+    @IBAction func slidersAction() {
+        redColorLabel.text = (
+            round(redColorSlider.value * 100) / 100.0
+        ).formatted()
+        
+        greenColorLabel.text = (
+            round(greenColorSlider.value * 100) / 100.0
+        ).formatted()
+        
+        blueColorLabel.text = (
+            round(blueColorSlider.value * 100) / 100.0
+        ).formatted()
+        
+        currentColorSwatch.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1
+        )
     }
-
 }
 
